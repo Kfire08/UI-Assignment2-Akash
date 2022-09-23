b297d2a739fb35d1398b1700f60b483b33ed8b7d
Akash Katiyar <akash.katiyar@automationanywhere.com>
2022-09-23 14:32:14 +0530

    test

--- script.js
+++ script.js
@@ -33,6 +33,19 @@ function search() {
   query = document.getElementById("searcharticle").value.toLowerCase();
   window.location.href = `?query=${query}`;
 }
+function myFunction() {
+  var copyText = document.getElementById("searcharticle");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
 
 document.getElementById("searcharticle").onkeydown = (e) => {
   if (e.key === "Enter") {
